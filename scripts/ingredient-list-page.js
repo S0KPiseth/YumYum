a229285f7f4f0b57de41e5e3122f8630da69e361
VannetNang <NangVannet59@gmail.com>
2025-11-25 20:34:37 +0700

    - (feat): implemented filter category function - added css responsive UI

--- scripts/ingredient-list-page.js
+++ scripts/ingredient-list-page.js
@@ -2,6 +2,43 @@ import { renderIngredientCard } from "../utils/render-ingredients-list.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderIngredientCard();
+
+  const filteredCategory = document.getElementById("filterSelectionForm");
+
+  filteredCategory.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    try {
+      const selected = document.querySelectorAll(
+        'input[type="radio"]:checked'
+      );
+
+      const ingredients =
+        JSON.parse(localStorage.getItem("ingredientsList")) || [];
+
+      selected.forEach((item) => {
+        if (item.value == "relevant") {
+          renderIngredientCard(ingredients);
+        }
+
+        if (item.value == "a-to-z") {
+          const aToZ = [...ingredients].sort((a, b) =>
+            a.strIngredient.localeCompare(b.strIngredient)
+          );
+          renderIngredientCard(aToZ);
+        }
+
+        if (item.value == "z-to-a") {
+          const zToA = [...ingredients].sort((a, b) =>
+            b.strIngredient.localeCompare(a.strIngredient)
+          );
+          renderIngredientCard(zToA);
+        }
+      });
+    } catch (error) {
+      console.log("Error", error);
+    }
+  });
 });
 
 window.searchIngredient = function (event) {
